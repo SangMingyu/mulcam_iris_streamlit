67fad57f17e92268b8e449ec0af28ca4c3d44638
Sang min gyu <ssangmg@gmail.com>
2023-09-01 10:16:51 +0900

    code update

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 # -*- coding:utf-8 -*-
 
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import scikitlearn as sklearn
-import plotly as px
 
 
 def main():
+
     st.markdown("# Hello World")
-    st.write(np.__version__),
-    st.write(pd.__version__),
-    st.write(plt.__version__),
-    st.write(sns.__version__),
-    st.write(sklearn.__version__),
-    st.write(px.__version__)
+
+    menu = ["Home", "탐색적 자료분석", "머신러닝", "About"]
+    choice = st.sidebar.selectbox("메뉴", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+    elif choice == "탐색적 자료분석":
+        st.subheader("탐색적 자료분석")
+    elif choice == "머신러닝":
+        st.subheader("머신러닝")
+    elif choice == "About":
+        st.subheader("About")
+    else:
+        pass
 
 if __name__ == "__main__":
     main()
