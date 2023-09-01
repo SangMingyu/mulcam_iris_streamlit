430904400e55fc07fdd6a4eafe1177dcfb70c131
Sang min gyu <ssangmg@gmail.com>
2023-09-01 10:06:48 +0900

    updatelib

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 import scikitlearn as sklearn
 import plotly as px
@@ -12,8 +12,11 @@ import plotly as px
 def main():
     st.markdown("# Hello World")
     st.write(np.__version__),
-    st.write(pd.__version__)
-    
+    st.write(pd.__version__),
+    st.write(plt.__version__),
+    st.write(sns.__version__),
+    st.write(sklearn.__version__),
+    st.write(px.__version__)
 
 if __name__ == "__main__":
     main()
