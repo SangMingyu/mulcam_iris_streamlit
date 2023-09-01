724fba21389e8e7473c411bf0fa529b241753f77
Sang min gyu <ssangmg@gmail.com>
2023-09-01 10:02:05 +0900

    updatelib

--- app.py
+++ app.py
@@ -12,11 +12,8 @@ import plotly as px
 def main():
     st.markdown("# Hello World")
     st.write(np.__version__),
-    st.write(pd.__version__),
-    st.write(plt.__version__),
-    st.write(sns.__version__),
-    st.write(sklearn.__version__),
-    st.write(px.__version__)
+    st.write(pd.__version__)
+    
 
 if __name__ == "__main__":
     main()
